f9c222f6aec53bbe1c812bab1139a7399364b45e
abi26anamo <abianamo282@gmail.com>
2023-12-10 18:48:50 +0300

    fixed errors in home

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -7,15 +7,14 @@ const Banner = () => {
       <div className="mx-auto px-8  flex flex-col gap-2 sm:flex-row justify-between">
         <div className="mb-8 sm:mb-0 text-center py-12">
           <h1 className="text-4xl sm:text-6xl font-bold text-white mb-4">
-            Discover new latest <br/>collections
+            Discover new latest <br />
+            collections
           </h1>
         </div>
-        <div className="sm:hidden md:w-1/3 h-1/3  relative aspect-video flex items-center">
-          <Image
-            src="/images/model.jpg"
-            fill
-            alt="Banner Image"
-          />
+        <div className="hidden md:block">
+          <div className=" md:w-1/3 h-1/3  relative aspect-video flex items-center">
+            <Image src="/images/model.jpg" fill alt="Banner Image" />
+          </div>
         </div>
       </div>
     </div>
